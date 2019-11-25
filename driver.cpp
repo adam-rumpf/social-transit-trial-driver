4121405ca69878188235e2fd930c65ded2682b33
Adam Rumpf <arumpf@hawk.iit.edu>
2019-11-25 14:48:06 -0500

    Initial commit of main driver.

---
+++ driver.cpp
@@ -0,0 +1,17 @@
+/**
+Driver for running sets of trials of the main solution algorithm.
+*/
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/// Main driver.
+int main()
+{
+	cout << "Hello, world!" << endl;
+
+	cin.get();
+}
