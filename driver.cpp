255f1ced734aa3b5cc16ce7f4f24b8e1398f3d1d
Adam Rumpf <arumpf@hawk.iit.edu>
2019-11-26 00:54:38 -0500

    Implemented trial logging.

--- driver.cpp
+++ driver.cpp
@@ -4,11 +4,6 @@ Driver for running sets of trials of the main solution algorithm.
 Must be placed in a folder alongside a trial file and the main solver exe.
 */
 
-/*
-To do:
--Include another trial option that specifies whether the trials are allowed to reuse the same solution log. If so, then we need a way to process the solution log to reassess feasibility in case the constraint parameter changes. We also need a way to get the solution log from one location to another.
-*/
-
 #include <cmath>
 #include <ctime>
 #include <fstream>
@@ -22,11 +17,13 @@ To do:
 // Problem parameter definitions
 #define SOLVER "social_transit_solver.exe"
 #define TRIAL_FILE "trials.txt"
+#define LOG_FILE "log.txt"
 
 using namespace std;
 
 // Function prototypes
 void load_trials();
+void write_log(bool, string, int, string);
 string s2hms(double);
 
 // Global variables
@@ -35,6 +32,8 @@ queue<tuple<string, string>> trial_queue; // queue of trials to process, each co
 /// Main driver.
 int main()
 {
+	bool first = true; // first iteration
+
 	// Load the trial set
 	load_trials();
 	int max_trials = trial_queue.size(); // number of trials to process
@@ -61,8 +60,16 @@ int main()
 		int exit_code = system(command.c_str()); // call solver and get exit code
 		double solution_time = (1.0*clock() - start) / CLOCKS_PER_SEC; // calculate solution time (in seconds)
 
+		// Print footer
 		cout << "\n\n\n----------------------------------------------------------------------" << endl;
-		cout << "Trial finished in " << s2hms(solution_time) << "\n\n\n";
+		cout << "Trial finished in " << s2hms(solution_time) << endl;
+
+		// Write to trial log
+		write_log(first, label, exit_code, s2hms(solution_time));
+
+		cout << "\n\n\n";
+
+		first = false;
 	}
 
 	cin.get();
@@ -104,6 +111,43 @@ void load_trials()
 		cout << "Trial file failed to open." << endl;
 		exit(1);
 	}
+}
+
+/**
+Writes to the trial log.
+
+Requires the following arguments, respectively:
+	-Boolean argument to specify whether this is the first iteration. If true, then the log is cleared and a comment line is written. If false, then the log is appended to.
+	-Trial label string.
+	-Trial exit code int.
+	-Trial execution time string.
+*/
+void write_log(bool first, string label, int code, string time)
+{
+	// If this is the first trial, clear the log and write a comment line
+	if (first == true)
+	{
+		ofstream log_file(LOG_FILE, ofstream::trunc);
+		if (log_file.is_open())
+		{
+			log_file << "label\texit\ttime" << endl;
+			log_file.close();
+			cout << "Reset log file." << endl;
+		}
+		else
+			cout << "Failed to reset log file." << endl;
+	}
+
+	// Write a new log file line
+	ofstream log_file(LOG_FILE, ofstream::app);
+	if (log_file.is_open())
+	{
+		log_file << label << '\t' << code << '\t' << time << endl;
+		log_file.close();
+		cout << "Logged trial." << endl;
+	}
+	else
+		cout << "Failed to log trial." << endl;
 }
 
 /// Converts a number of seconds to an HH:MM:SS string.
